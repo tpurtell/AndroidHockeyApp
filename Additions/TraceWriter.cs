1af5d52a3ab074ca7f82f36d3cfc5159b3905283
T.J. Purtell <tpurtell@gmail.com>
2013-07-12 16:10:24 -0700

    dont reference java objects or a domain wide exception handler will null reference whenever it accesses a java object.

--- Additions/TraceWriter.cs
+++ Additions/TraceWriter.cs
@@ -1,10 +1,8 @@
 // public domain ... derived from https://github.com/bitstadium/HockeySDK-Android/blob/db7fff12beecea715f2894cb69ba358ea324ad17/src/main/java/net/hockeyapp/android/internal/ExceptionHandler.java
 using System;
+using System.Diagnostics;
 using System.IO;
-using Java.Lang;
-using Java.Util;
-using Exception = System.Exception;
-using Process = Android.OS.Process;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 
 namespace Net.Hockeyapp.Android
 {
@@ -14,7 +12,9 @@ namespace Net.Hockeyapp.Android
         {
             DateTime date = DateTime.UtcNow;
             // Create filename from a random uuid
-            string filename = UUID.RandomUUID().ToString();
+            var bytes = new byte[16];
+            new Random().NextBytes(bytes);
+            string filename = new SoapHexBinary(bytes).ToString();
             string path = Path.Combine(Constants.FilesPath, filename + ".stacktrace");
             Console.WriteLine("Writing unhandled exception to: {0}", path);
             try
@@ -49,8 +49,7 @@ namespace Net.Hockeyapp.Android
             {
                 Console.WriteLine("Error saving exception stacktrace! {0}", another);
             }
-            Process.KillProcess(Process.MyPid());
-            JavaSystem.Exit(10);
+            Process.GetCurrentProcess().Kill();
             Environment.Exit(10);
         }
     }
