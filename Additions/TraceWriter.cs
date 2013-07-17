c7a5d849240521e2932140fa28f6581d70891d00
T.J. Purtell <tpurtell@gmail.com>
2013-07-17 13:00:00 -0700

    incorporate direct usage of hockeyapp's java save exception where applicable.  update C# trace writer to include the other fields supported by hockey app.

--- Additions/TraceWriter.cs
+++ Additions/TraceWriter.cs
@@ -3,34 +3,47 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
 
 namespace Net.Hockeyapp.Android
 {
     public static class TraceWriter
-    {        
-        private static CrashManagerListener _listener;
+    {
+        private static CrashManagerListener _Listener;
 
-        private static string _appPackage = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
-        private static string _appVersion = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
-        private static string _androidVersion = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
-        private static string _phoneManufacturer = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
-        private static string _phoneModel = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
-        private static string _filesPath = ".";
+        private static string _AppPackage = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
+        private static string _AppVersion = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
+        private static string _AndroidVersion = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
+        private static string _PhoneManufacturer = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
+        private static string _PhoneModel = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
+        private static string _FilesPath = ".";
+        private static bool _IncludeDeviceData = true;
+        private static string _User = "Unknown: call TraceWriter.Initialize(listener) after CrashManager.Initialize";
+        private static string _Contact = "Unknown: call TraceWriter.Initialize(listener) after CrashManager.Initialize";
+        private static string _Description = "Unknown: call TraceWriter.Initialize(listener) after CrashManager.Initialize";
 
         /// <summary>
-        /// Copy build properties into c# land so that the handler won't crash accessing java.
+        /// Copy build properties into c# land so that the handler won't crash accessing java.  When an unhandled exception occurs
+        /// in a backgroud task or thread that is owned by the c# runtime, Xamarin.Android crashes if you try to invoke anything
+        /// inside the java runtime.
+        /// see https://bugzilla.xamarin.com/show_bug.cgi?id=10379 for more info
         /// </summary>
         public static void InitializeConstants()
         {
-            _appPackage = Constants.AppPackage;
-            _appVersion = Constants.AppVersion;
-            _androidVersion = Constants.AndroidVersion;
-            _phoneManufacturer = Constants.PhoneManufacturer;
-            _phoneModel = Constants.PhoneModel;
-            _filesPath = Constants.FilesPath;
+            _AppPackage = Constants.AppPackage;
+            _AppVersion = Constants.AppVersion;
+            _AndroidVersion = Constants.AndroidVersion;
+            _PhoneManufacturer = Constants.PhoneManufacturer;
+            _PhoneModel = Constants.PhoneModel;
+            _FilesPath = Constants.FilesPath;
         }
 
-        //ToDo: Check if static variables survive lifecycles of different activities!
+        private static string ClipString(string s)
+        {
+            if ((s != null) && (s.Length > 255))
+                s = s.Substring(0, 255);
+            return s;
+        }
         /// <summary>
         /// Initialize the TraceWriter with a given CrashManagerListener
         /// </summary>
@@ -38,70 +51,99 @@ namespace Net.Hockeyapp.Android
         /// <remarks>Thuis is important to use the UserID, Contact and Description from CrashManagerListener.</remarks>
         public static void Initialize(CrashManagerListener listener)
         {
-            _listener = listener;
+            _Listener = listener;
             InitializeConstants();
+            if (listener != null)
+            {
+                _IncludeDeviceData = _Listener.IncludeDeviceData();
+                _User = ClipString(listener.UserID);
+                _Contact = ClipString(listener.Contact);
+                _Description = listener.Description;
+            }
+        }
+        public static void WriteTrace(object exception)
+        {
+            WriteTrace(exception, true);
         }
 
         /// <summary>
         /// Writes the given object (usually an exception) to disc so that it can be picked up by the CrashManager and send to Hockeyapp.
         /// </summary>
         /// <param name="exception">The object to write (usually an exception)</param>
-        /// <remarks>This method controls exactly what is written to disc. UserID, Contact and Description from CrashManagerListener are NOT used!</remarks>
-        public static void WriteTrace(object exception)
+        /// <param name="terminate">Flag that determines whether the process should be terminated after logging the exception</param>
+        /// <remarks>This method controls exactly what is written to disc.  Its really a translation of the ExceptionHandler.saveException
+        /// This method doesn't call any java if the exception object isn't a java class so it is safe to call when the mono runtime is no 
+        /// longer able to invoke JNI methods, for example in the case of a crash in the pure c# managed space.</remarks>
+        public static void WriteTrace(object exception, bool terminate)
         {
-            var date = DateTime.Now;
-            // Create filename from a random uuid
-            //ToDo: Why not use Guid.NewGuid().ToString() ???            
-            var bytes = new byte[16];
-            new Random().NextBytes(bytes);
-            var filename = new SoapHexBinary(bytes).ToString();
-            var path = Path.Combine(_filesPath, filename + ".stacktrace");
-            Console.WriteLine("Writing unhandled exception to: {0}", path);
-            try
+            //if the exception came from java, then we should be able to invoke it
+            if (exception is Java.Lang.Exception)
             {
-                using (var f = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None))
-                using (var sw = new StreamWriter(f))
+                ExceptionHandler.SaveException(exception as Java.Lang.Exception, _Listener);
+            }
+            else
+            {
+                var date = DateTime.Now;
+                var filename = Guid.NewGuid().ToString();
+                var path = Path.Combine(_FilesPath, filename + ".stacktrace");
+                Console.WriteLine("Writing unhandled exception to: {0}", path);
+                try
                 {
-                    // Write the stacktrace to disk
-                    sw.WriteLine("Package: {0}", _appPackage);
-                    sw.WriteLine("Version: {0}", _appVersion);
-                    sw.WriteLine("Android: {0}", _androidVersion);
-                    sw.WriteLine("Manufacturer: {0}", _phoneManufacturer);
-                    sw.WriteLine("Model: {0}", _phoneModel);
-                    sw.WriteLine("Date: {0}", date);
-                    sw.WriteLine();
-                    try
+                    using (var f = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None))
+                    using (var sw = new StreamWriter(f))
                     {
-                        sw.WriteLine(exception);
-                    }
-                    catch (Exception e)
-                    {
-                        // checking for https://bugzilla.xamarin.com/show_bug.cgi?id=10379
-                        // null reference exceptions with no stack trace from inside unhandled exception handler
-                        // may not be a problem, investigating.
+                        // Write the stacktrace to disk
+                        sw.WriteLine("Package: {0}", _AppPackage);
+                        sw.WriteLine("Version: {0}", _AppVersion);
+                        if (_IncludeDeviceData)
+                        {
+                            sw.WriteLine("Android: {0}", _AndroidVersion);
+                            sw.WriteLine("Manufacturer: {0}", _PhoneManufacturer);
+                            sw.WriteLine("Model: {0}", _PhoneModel);
+                        }
+                        sw.WriteLine("Date: {0}", date);
                         sw.WriteLine();
-                        sw.WriteLine("Exception writing exception: {0}", e);
-                        throw new Exception("Problem writing exception", e);
+                        try
+                        {
+                            sw.WriteLine(exception);
+                        }
+                        catch (Exception e)
+                        {
+                            // log something just in case something triggers the null refrence with the attached exception
+                            // see https://bugzilla.xamarin.com/show_bug.cgi?id=10379
+                            sw.WriteLine();
+                            sw.WriteLine("Exception writing exception: {0}", e);
+                            throw new Exception("Problem writing exception", e);
+                        }
+                        if (_Listener != null)
+                        {
+                            File.WriteAllText(Path.Combine(_FilesPath, filename + ".user"), ClipString(_User),
+                                              Encoding.UTF8);
+                            File.WriteAllText(Path.Combine(_FilesPath, filename + ".contact"), ClipString(_Contact),
+                                              Encoding.UTF8);
+                            File.WriteAllText(Path.Combine(_FilesPath, filename + ".description"), _Description,
+                                              Encoding.UTF8);
+                        }
                     }
                 }
-            }
-            catch (Exception another)
-            {
-                Console.WriteLine("Error saving exception stacktrace! {0}", another);
+                catch (Exception another)
+                {
+                    Console.WriteLine("Error saving exception stacktrace! {0}", another);
+                }
             }
             Process.GetCurrentProcess().Kill();
             Environment.Exit(10);
         }
 
         /// <summary>
-        /// Writes the given exception to disc using the standard Exceptionhandler.
+        /// Writes the given exception to disc using the standard Exceptionhandler.  Does not terminate the process, so it could be suitable
+        /// for reporting non-fatal logs that upload on the next CrashManager execution
         /// </summary>
         /// <param name="exception">The exception to write.</param>
         /// <remarks>All features from CrashManagerListener are used (including informations like UserId, Contact and Description).</remarks>
         public static void WriteTraceAndInfo(Exception exception)
         {
-            var throwable = Java.Lang.Throwable.FromException(exception);
-            ExceptionHandler.SaveException(new Java.Lang.Throwable(throwable), _listener);
+            WriteTrace(exception, false);
         }
 
     }
