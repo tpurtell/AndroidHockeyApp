cc9bbcce96193d7083b241218cdf47fda86839fc
Dirk Schiffner <ds@adestis.de>
2013-07-17 15:03:35 +0200

    Added support for CrashManagerListener
    Added support for CrashManagerListener in TraceWriter

--- Additions/TraceWriter.cs
+++ Additions/TraceWriter.cs
@@ -7,31 +7,55 @@ using System.Runtime.Remoting.Metadata.W3cXsd2001;
 namespace Net.Hockeyapp.Android
 {
     public static class TraceWriter
-    {
-        private static string _AppPackage = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
-        private static string _AppVersion = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
-        private static string _AndroidVersion = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
-        private static string _PhoneManufacturer = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
-        private static string _PhoneModel = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
-        private static string _FilesPath = ".";
+    {        
+        private static CrashManagerListener _listener;
 
+        private static string _appPackage = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
+        private static string _appVersion = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
+        private static string _androidVersion = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
+        private static string _phoneManufacturer = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
+        private static string _phoneModel = "Unknown: call TraceWriter.InitializeConstants after CrashManager.Initialize";
+        private static string _filesPath = ".";
+
+        /// <summary>
+        /// Copy build properties into c# land so that the handler won't crash accessing java.
+        /// </summary>
         public static void InitializeConstants()
         {
-            _AppPackage = Constants.AppPackage;
-            _AppVersion = Constants.AppVersion;
-            _AndroidVersion = Constants.AndroidVersion;
-            _PhoneManufacturer = Constants.PhoneManufacturer;
-            _PhoneModel = Constants.PhoneModel;
-            _FilesPath = Constants.FilesPath;
+            _appPackage = Constants.AppPackage;
+            _appVersion = Constants.AppVersion;
+            _androidVersion = Constants.AndroidVersion;
+            _phoneManufacturer = Constants.PhoneManufacturer;
+            _phoneModel = Constants.PhoneModel;
+            _filesPath = Constants.FilesPath;
         }
+
+        //ToDo: Check if static variables survive lifecycles of different activities!
+        /// <summary>
+        /// Initialize the TraceWriter with a given CrashManagerListener
+        /// </summary>
+        /// <param name="listener"></param>
+        /// <remarks>Thuis is important to use the UserID, Contact and Description from CrashManagerListener.</remarks>
+        public static void Initialize(CrashManagerListener listener)
+        {
+            _listener = listener;
+            InitializeConstants();
+        }
+
+        /// <summary>
+        /// Writes the given object (usually an exception) to disc so that it can be picked up by the CrashManager and send to Hockeyapp.
+        /// </summary>
+        /// <param name="exception">The object to write (usually an exception)</param>
+        /// <remarks>This method controls exactly what is written to disc. UserID, Contact and Description from CrashManagerListener are NOT used!</remarks>
         public static void WriteTrace(object exception)
         {
-            DateTime date = DateTime.Now;
+            var date = DateTime.Now;
             // Create filename from a random uuid
+            //ToDo: Why not use Guid.NewGuid().ToString() ???            
             var bytes = new byte[16];
             new Random().NextBytes(bytes);
-            string filename = new SoapHexBinary(bytes).ToString();
-            string path = Path.Combine(_FilesPath, filename + ".stacktrace");
+            var filename = new SoapHexBinary(bytes).ToString();
+            var path = Path.Combine(_filesPath, filename + ".stacktrace");
             Console.WriteLine("Writing unhandled exception to: {0}", path);
             try
             {
@@ -39,11 +63,11 @@ namespace Net.Hockeyapp.Android
                 using (var sw = new StreamWriter(f))
                 {
                     // Write the stacktrace to disk
-                    sw.WriteLine("Package: {0}", _AppPackage);
-                    sw.WriteLine("Version: {0}", _AppVersion);
-                    sw.WriteLine("Android: {0}", _AndroidVersion);
-                    sw.WriteLine("Manufacturer: {0}", _PhoneManufacturer);
-                    sw.WriteLine("Model: {0}", _PhoneModel);
+                    sw.WriteLine("Package: {0}", _appPackage);
+                    sw.WriteLine("Version: {0}", _appVersion);
+                    sw.WriteLine("Android: {0}", _androidVersion);
+                    sw.WriteLine("Manufacturer: {0}", _phoneManufacturer);
+                    sw.WriteLine("Model: {0}", _phoneModel);
                     sw.WriteLine("Date: {0}", date);
                     sw.WriteLine();
                     try
@@ -57,7 +81,7 @@ namespace Net.Hockeyapp.Android
                         // may not be a problem, investigating.
                         sw.WriteLine();
                         sw.WriteLine("Exception writing exception: {0}", e);
-                        throw e;
+                        throw new Exception("Problem writing exception", e);
                     }
                 }
             }
@@ -68,5 +92,17 @@ namespace Net.Hockeyapp.Android
             Process.GetCurrentProcess().Kill();
             Environment.Exit(10);
         }
+
+        /// <summary>
+        /// Writes the given exception to disc using the standard Exceptionhandler.
+        /// </summary>
+        /// <param name="exception">The exception to write.</param>
+        /// <remarks>All features from CrashManagerListener are used.</remarks>
+        public static void WriteTrace(Exception exception)
+        {
+            var throwable = Java.Lang.Throwable.FromException(exception);
+            ExceptionHandler.SaveException(new Java.Lang.Throwable(throwable), _listener);
+        }
+
     }
 }
